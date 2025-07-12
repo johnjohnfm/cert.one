fbe44194e8c2336a3ade99c29d6995701b5d1320
JOHNJOHNFM <158992033+johnjohnfm@users.noreply.github.com>
2025-07-12 05:21:16 -0500

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Install Chrome dependencies
+apt-get update && apt-get install -y \
+  wget \
+  gnupg \
+  ca-certificates \
+  fonts-liberation \
+  libasound2 \
+  libatk-bridge2.0-0 \
+  libatk1.0-0 \
+  libcups2 \
+  libdbus-1-3 \
+  libdrm2 \
+  libgbm1 \
+  libgtk-3-0 \
+  libnspr4 \
+  libnss3 \
+  libxcomposite1 \
+  libxdamage1 \
+  libxfixes3 \
+  libxkbcommon0 \
+  libxrandr2 \
+  xdg-utils \
+  libu2f-udev \
+  libvulkan1 \
+  libxss1 \
+  --no-install-recommends
+
+# Add Google Chrome repository
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list
+
+# Install Google Chrome
+apt-get update
+apt-get install -y google-chrome-stable --no-install-recommends
+
+# Clean up
+rm -rf /var/lib/apt/lists/*
+
+# Install Node dependencies
+npm install --production
